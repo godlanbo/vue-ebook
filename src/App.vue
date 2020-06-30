d25d02d3cfd9bf402969a5260b139daa22b9e16d
GodLanBo <912300762@qq.com>
2020-06-30 15:09:27 +0800

    添加资源文件

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,29 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div class="text">ABCD</div>
     </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+window.addEventListener('DOMContentLoaded', () => {
+  let html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+export default {
+  mounted () {
+    this.$store.dispatch('setTest', 10).then(() => {
+      console.log(this.$store.state.book.test)
+    })
+  }
 }
-
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+</script>
+<style lang="scss">
+@import './assets/style/global.scss';
+.text {
+  font-size: px2rem(37.5);
 }
 </style>
